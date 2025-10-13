a8047e062209197be776413ce68f5195613e0844
izuzupucha <izuzupucha@gmail.com>
2025-10-13 20:26:35 +0700

    heathcheck

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def is_running_on_streamlit_cloud() -> bool:
     return st.secrets.get("env", {}).get("mode") == "cloud"
 
 def main():
+    params = st.experimental_get_query_params()
+    if "ping" in params:
+        st.write(f"OK - {datetime.datetime.utcnow().isoformat()} UTC")
+        st.stop()
+    
     user_controller = UserController()
     controller = CoinController()
 
