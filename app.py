ce04c880b0b97c6a54713bb16a16dbeedc65d8ad
izuzupucha <izuzupucha@gmail.com>
2025-10-13 21:11:27 +0700

    heathcheck

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-import os
+import sys, os
+sys.path.append(os.path.dirname(__file__))
 import streamlit as st
 from controller.coin_controller import CoinController
 from controller.user_controller import UserController
